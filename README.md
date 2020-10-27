63632d1cb3d7e8bfcfbdc478c04d60203069762c
nataprawiraf <nataprawiraf@gmail.com>
2020-10-28 00:32:19 +0700

    adding readme and fix port on heroku

---
+++ README.md
@@ -0,0 +1,8 @@
+## Site and Heroku Remote
+https://stark-garden-64765.herokuapp.com/ | https://git.heroku.com/stark-garden-64765.git
+
+## Source
+
+- https://devcenter.heroku.com/articles/node-websockets
+- https://github.com/heroku-examples/node-socket.io
+- https://elements.heroku.com/buttons/socketio/chat-example
--- server.js
+++ server.js
@@ -3,33 +3,33 @@ const http = require("http").Server(app);
 const io = require("socket.io")(http);
 
 app.get("/", function (req, res) {
-  res.sendfile("index.html");
+    res.sendfile("index.html");
 });
 
 users = [];
 io.on("connection", function (socket) {
-  console.log("A user connected");
-  socket.on("setUsername", function (data) {
-    console.log(data);
-    if (data === "") {
-      socket.emit("userBlank", data + " username can not be blank!");
-    } else if (users.indexOf(data) > -1) {
-      socket.emit(
-        "userExists",
-        data + " username is taken! Try some other username."
-      );
-    } else {
-      users.push(data);
-      socket.emit("userSet", { username: data });
-    }
-  });
+    console.log("A user connected");
+    socket.on("setUsername", function (data) {
+        console.log(data);
+        if (data === "") {
+            socket.emit("userBlank", data + " username can not be blank!");
+        } else if (users.indexOf(data) > -1) {
+            socket.emit(
+                "userExists",
+                data + " username is taken! Try some other username."
+            );
+        } else {
+            users.push(data);
+            socket.emit("userSet", {username: data});
+        }
+    });
 
-  socket.on("msg", function (data) {
-    //Send message to everyone
-    io.sockets.emit("newmsg", data);
-  });
+    socket.on("msg", function (data) {
+        //Send message to everyone
+        io.sockets.emit("newmsg", data);
+    });
 });
 
-http.listen(3000, function () {
-  console.log("listening on localhost:3000");
+http.listen(process.env.PORT || 5000, function () {
+    console.log("listening on localhost:3000");
 });
