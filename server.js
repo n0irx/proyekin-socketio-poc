60b6a24b163b1bef7877771ce833015314449f8e
nataprawiraf <nataprawiraf@gmail.com>
2020-10-27 20:53:03 +0700

    adding firsti init project

---
+++ server.js
@@ -0,0 +1,35 @@
+const app = require("express")();
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
+
+app.get("/", function (req, res) {
+  res.sendfile("index.html");
+});
+
+users = [];
+io.on("connection", function (socket) {
+  console.log("A user connected");
+  socket.on("setUsername", function (data) {
+    console.log(data);
+    if (data === "") {
+      socket.emit("userBlank", data + " username can not be blank!");
+    } else if (users.indexOf(data) > -1) {
+      socket.emit(
+        "userExists",
+        data + " username is taken! Try some other username."
+      );
+    } else {
+      users.push(data);
+      socket.emit("userSet", { username: data });
+    }
+  });
+
+  socket.on("msg", function (data) {
+    //Send message to everyone
+    io.sockets.emit("newmsg", data);
+  });
+});
+
+http.listen(3000, function () {
+  console.log("listening on localhost:3000");
+});
