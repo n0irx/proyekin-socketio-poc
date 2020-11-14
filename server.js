06eb944467091f31360397ef6dda7fe6263e9875
nataprawiraf <nataprawiraf@gmail.com>
2020-11-14 20:17:50 +0700

    initialize

--- server.js
+++ server.js
@@ -1,35 +1,36 @@
-const app = require("express")();
-const http = require("http").Server(app);
-const io = require("socket.io")(http);
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
-app.get("/", function (req, res) {
-    res.sendfile("index.html");
+app.get('/', function (req, res) {
+    res.sendfile('index.html');
 });
 
 users = [];
-io.on("connection", function (socket) {
-    console.log("A user connected");
-    socket.on("setUsername", function (data) {
+io.on('connection', function (socket) {
+    console.log('A user connected');
+
+    socket.on('setUsername', function (data) {
         console.log(data);
-        if (data === "") {
-            socket.emit("userBlank", data + " username can not be blank!");
-        } else if (users.indexOf(data) > -1) {
+        if (data === '') {
+            socket.emit('userBlank', data + ' username can not be blank!');
+        } else if (users.indexOf(data[['username']]) > -1) {
             socket.emit(
-                "userExists",
-                data + " username is taken! Try some other username."
+                'userExists',
+                data + ' username is taken! Try some other username.'
             );
         } else {
-            users.push(data);
-            socket.emit("userSet", {username: data});
+            users.push(data['username']);
+            socket.emit('userSet', { username: data });
         }
     });
 
-    socket.on("msg", function (data) {
+    socket.on('msg', function (data) {
         //Send message to everyone
-        io.sockets.emit("newmsg", data);
+        io.sockets.emit('newmsg', data);
     });
 });
 
 http.listen(process.env.PORT || 5000, function () {
-    console.log("listening on localhost:3000");
+    console.log('listening on localhost:3000');
 });
